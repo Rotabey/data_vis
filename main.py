3bdc5773eb9a6a7e3e80c5ee38ab0dee06c6a4b0
Rotabey <jbaptiste.cherrier.pro@gmail.com>
2024-03-31 05:55:05 +0200

    v2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from utils import scatter_plot_for_year, plot_top_10_ranks_evolution, plot_map, plot_top_countries_map
+from utils import scatter_plot_for_year, plot_top_10_ranks_evolution, plot_map, plot_top_countries_map, \
+    plot_percentage_ring
 import pandas as pd
 import geopandas as gpd
 
@@ -9,22 +10,35 @@ data.loc[data['Country'] == 'Russian Federation', 'Country'] = 'Russia'
 world = gpd.read_file('data/ne_110m_admin_0_countries/ne_110m_admin_0_countries.shp')
 
 st.set_page_config(layout="wide")
-
-st.sidebar.title('Filtres')
-selected_year_slider_key = "selected_year_slider"
-selected_year = st.sidebar.slider('Sélectionner une année', min_value=data['Year'].min(), max_value=data['Year'].max(),
-                                  value=data['Year'].min(), key=selected_year_slider_key)
-
+st.title('Publications scientifiques par pays')
+st.write('')
 tabs = st.tabs(["Nuage de Points", "Évolution des Rangs", "Carte Top 10", "Carte H-index"])
 
+# Afficher le nuage de points avec les filtres appliqués
 with tabs[0]:
     st.markdown(
-        "<h2 style='font-weight:bold;margin-bottom:20px;font-size:35px'>Nuage de Points: Documents vs Citations pour " + str(
-            selected_year) + "</h2>", unsafe_allow_html=True)
-    col1, col2 = st.columns([3, 1], gap="large")
+        "<h2 style='font-weight:bold;margin-bottom:20px;font-size:35px'>Nuage de Points: Documents vs Citations</h2>",
+        unsafe_allow_html=True)
+    col1, col2, col3 = st.columns([1, 3, 1], gap="large")
     with col1:
-        scatter_plot_for_year(data, selected_year)
+        st.subheader('Filtres')
+        selected_year_slider_key = "selected_year_slider"
+        selected_year = st.slider('Sélectionner une année', min_value=data['Year'].min(), max_value=data['Year'].max(),
+                                  value=data['Year'].min(), key=selected_year_slider_key)
+        min_docs_filter, max_docs_filter = st.slider("Nombre de documents (fourchette)",
+                                                     min_value=0,
+                                                     max_value=400000,
+                                                     step=1000,
+                                                     value=(0, 400000))
+        min_citations_filter, max_citations_filter = st.slider("Nombre de citations (fourchette)",
+                                                               min_value=0,
+                                                               max_value=11000000,
+                                                               step=1000,
+                                                               value=(0, 11000000))
     with col2:
+        scatter_plot_for_year(data, selected_year, min_docs_filter, max_docs_filter, min_citations_filter,
+                              max_citations_filter)
+    with col3:
         st.subheader('Le leader :')
         st.write(
             "On peut constater à travers ce graphique que les États-Unis sont, de 1996 à 2014, le pays qui arrive à produire le plus de documents qui sont "
@@ -48,30 +62,57 @@ with tabs[1]:
 
 with tabs[2]:
     st.markdown(
-        "<h2 style='font-weight:bold;margin-bottom:20px;font-size:35px'>Carte des pays du top 10 pour l\'année " + str(
-            selected_year) + " selon le nombre de documents produits</h2>", unsafe_allow_html=True)
-    col1, col2 = st.columns([3, 1], gap="large")
+        "<h2 style='font-weight:bold;margin-bottom:20px;font-size:35px'>Carte des pays du top 10 selon le nombre de documents produits</h2>",
+        unsafe_allow_html=True)
+    col1, col2, col3 = st.columns([1, 3, 1], gap="large")
     with col1:
-        plot_top_countries_map(data, world, 10, selected_year)
+        st.subheader('Filtres')
+        selected_year_slider_key_2 = "selected_year_slider_2"  # Utilisez une clé distincte
+        selected_year_2 = st.slider('Sélectionner une année', min_value=data['Year'].min(),
+                                    max_value=data['Year'].max(), value=data['Year'].min(),
+                                    key=selected_year_slider_key_2)
     with col2:
-        st.write(
-            "On déduit à partir de ce graphique que le classement du top 10 des pays reste le même de 1996 à 2014. "
-            "Sauf à un moment où l'Australie intègre ce classement à la 10ème position entre 2004 et 2007 à la place de la Russie. "
-            "Puis la 10ème position est prise en 2008 par l'Inde et cela jusqu'en 2014.")
+        plot_top_countries_map(data, world, 10, selected_year_2)
+    with col3:
+        st.subheader('Classement des 10 premiers pays par rang ')
+
+        # Filtrer les données pour l'année sélectionnée et sélectionner les 10 premiers pays par rang
+        filtered_data = data[data['Year'] == selected_year_2][['Country', 'Rank', 'Documents']].sort_values(
+            by='Rank').head(
+            10).reset_index(drop=True)
+
+        # Convertir le DataFrame en HTML en masquant les index
+        html_table = filtered_data.to_html(index=False)
+
+        # Afficher le tableau HTML dans Streamlit
+        st.markdown(html_table, unsafe_allow_html=True)
 
 with tabs[3]:
-    h_index_slider_key = "h_index_threshold_slider"
-    h_index_threshold = st.sidebar.slider('Sélectionner un seuil pour l\'indice H', min_value=0,
-                                          max_value=data['H.index'].max(), value=0, key=h_index_slider_key)
     st.markdown(
-        "<h2 style='font-weight:bold;margin-bottom:20px;font-size:35px'>Carte des pays avec un H-index supérieur à " + str(
-            h_index_threshold) + " pour l'année " + str(selected_year) + "</h2>", unsafe_allow_html=True)
-    col1, col2 = st.columns([3, 1], gap="large")
+        "<h2 style='font-weight:bold;margin-bottom:20px;font-size:35px'>Carte des pays selon le H-index</h2>",
+        unsafe_allow_html=True)
+    col1, col2, col3 = st.columns([1, 3, 1], gap="large")
     with col1:
-        plot_map(selected_year, h_index_threshold, data, world)
+        st.subheader('Filtres')
+        selected_year_slider_key_3 = "selected_year_slider_3"
+        selected_year_3 = st.slider('Sélectionner une année', min_value=data['Year'].min(),
+                                    max_value=data['Year'].max(), value=data['Year'].min(),
+                                    key=selected_year_slider_key_3)
+
+        # Ajout du filtre pour l'indice H
+        h_index_slider_key = "h_index_slider"
+        h_index_range = st.slider('Sélectionner une fourchette pour l\'indice H',
+                                  min_value=data['H.index'].min(),
+                                  max_value=data['H.index'].max(),
+                                  value=(data['H.index'].min(), data['H.index'].max()),
+                                  key=h_index_slider_key)
+
     with col2:
-        st.write(
-            "On se rend compte qu'à partir de ce graphique que la plupart des pays occidentaux sont performants en termes de production scientifique et d'impact."
-            "En Asie, on remarque que les pays les plus productifs sont la Chine, l'Inde, le Japon et la Russie."
-            "En Amérique, les États-Unis sont leaders suivis par le Canada, le Groenland, le Brésil, le Mexique et l'Argentine. Le reste des pays est moins productif, notamment en Amérique latine."
-            "Concernant l'Afrique, les pays sont peu performants en production de documents et ces derniers ne sont pas énormément cités, sauf pour l'Afrique du Sud.")
+        plot_map(selected_year_3, h_index_range[0], h_index_range[1], data,
+                 world)  # Passer les valeurs des filtres à la fonction plot_map
+    with col3:
+        st.subheader(
+            f'Le pourcentage de pays qui ont un H-index entre  {h_index_range[0]} et {h_index_range[1]} pour l\'année {selected_year_3}')
+        fig = plot_percentage_ring(selected_year_3, h_index_range[0], h_index_range[1],
+                                   data)  # Appeler la fonction avec les bons paramètres
+        st.pyplot(fig)  # Afficher la figure à l'aide de st.pyplot
--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import streamlit as st
 
 
-def scatter_plot_for_year(data, year):
+def scatter_plot_for_year(data, year, min_documents, max_documents, min_citations, max_citations):
     # Filtrer les données pour l'année sélectionnée
     data_year_filtered = data[data['Year'] == year]
 
@@ -15,8 +15,16 @@ def scatter_plot_for_year(data, year):
         avg_h_index=('H.index', 'mean')
     ).reset_index()
 
+    # Filtrer les pays selon la fourchette de nombre de documents et de citations
+    grouped_data_for_year_filtered = grouped_data_for_year[
+        (grouped_data_for_year['avg_documents'] >= min_documents) &
+        (grouped_data_for_year['avg_documents'] <= max_documents) &
+        (grouped_data_for_year['avg_citations'] >= min_citations) &
+        (grouped_data_for_year['avg_citations'] <= max_citations)
+        ]
+
     # Nuage de points pour l'année sélectionnée
-    scatter_plot = alt.Chart(grouped_data_for_year).mark_circle().encode(
+    scatter_plot = alt.Chart(grouped_data_for_year_filtered).mark_circle().encode(
         x=alt.X('avg_documents', title='Moyenne des documents'),
         y=alt.Y('avg_citations', title='Moyenne des citations'),
         color=alt.Color('median_rank', scale=alt.Scale(scheme='category20b'), title='Rang'),
@@ -49,22 +57,67 @@ def plot_top_10_ranks_evolution(top_10_ranks):
     st.write(chart)
 
 
-def plot_map(year, h_index, data, world):
-    data_filtered = data[(data['Year'] == year) & (data['H.index'] >= h_index)]
+def plot_map(selected_year, min_h_index, max_h_index, data, world):
+    # Filtrer les données en fonction de l'année et de la plage d'indices H
+    data_filtered = data[
+        (data['Year'] == selected_year) & (data['H.index'] >= min_h_index) & (data['H.index'] <= max_h_index)]
     world_filtered = world.merge(data_filtered, how='left', left_on='SOVEREIGNT', right_on='Country')
 
+    # Créer la carte
     fig, ax = plt.subplots(1, 1, figsize=(15, 10))
     world.plot(ax=ax, color='black', edgecolor='black')
     world_filtered.plot(ax=ax, column='H.index', cmap='viridis', edgecolor='black', legend=True)
 
+    # Ajouter une légende à la carte
     if not ax.collections:
-        vmin = h_index
-        vmax = data[
-            'H.index'].max()  # Utiliser la valeur maximale de l'ensemble des données comme valeur maximale de l'échelle
+        vmin = min_h_index
+        vmax = max_h_index
         norm = plt.Normalize(vmin=vmin, vmax=vmax)
         cbar = plt.colorbar(plt.cm.ScalarMappable(norm=norm, cmap='viridis'), ax=ax)
         cbar.set_label('Indice H')
+
+    # Afficher la carte dans Streamlit
     st.pyplot(fig)
+
+
+def plot_percentage_ring(selected_year, min_h_index, max_h_index, data):
+    # Filtrer les données en fonction de l'année et de la fourchette d'indices H
+    data_filtered = data[
+        (data['Year'] == selected_year) & (data['H.index'] >= min_h_index) & (data['H.index'] <= max_h_index)
+        ]
+
+    # Calculer le nombre de pays dans la fourchette
+    num_countries_in_range = len(data_filtered)
+
+    # Calculer le nombre total de pays
+    total_countries = len(data[data['Year'] == selected_year]['Country'].unique())
+
+    # Calculer le pourcentage de pays dans la fourchette par rapport au nombre total de pays
+    percentage_in_range = (num_countries_in_range / total_countries) * 100
+
+    # Couleurs plus foncées pour les deux sections de l'anneau
+    colors = ['#2ca02c', '#d62728']
+
+    # Tailles des sections de l'anneau
+    sizes = [percentage_in_range, 100 - percentage_in_range]
+
+    fig, ax = plt.subplots(facecolor='#0E1117')  # Couleur de fond personnalisée
+    wedges, texts, autotexts = ax.pie(sizes, colors=colors, startangle=90, wedgeprops=dict(width=0.3),
+                                      autopct='%1.1f%%')
+    ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+    # Modifier la taille et la couleur du texte pour les pourcentages
+    for autotext in autotexts:
+        autotext.set_fontsize(14)
+        autotext.set_color('white')
+        # Supprimer les étiquettes 0% et 100%
+        if autotext.get_text() in ['0.0%', '100.0%']:
+            autotext.set_text('')
+
+    # Ajouter un texte au centre de l'anneau
+    centre_text = ax.text(0, 0, f"{percentage_in_range:.1f}%", ha='center', va='center', fontsize=40, color='white')
+
+    return fig
 
 
 def plot_top_countries_map(data, world, top_n, selected_year):
@@ -83,6 +136,10 @@ def plot_top_countries_map(data, world, top_n, selected_year):
     # Colorer les pays en fonction du nombre de documents produits
     merged_data.plot(ax=ax, column='Documents', cmap='viridis', legend=True)
     plt.axis('off')
+
+    # Récupérer la légende de la carte
+    cbar = ax.get_figure().get_axes()[1]
+    cbar.set_ylabel('Nombre de documents produits', fontsize=12)
 
     # Afficher la carte dans Streamlit
     st.pyplot(fig)
